38a23573e25002bd00d061035e0985dbda60c541
YangWeiBin <863255386@qq.com>
2021-11-15 22:47:20 +0800

    20211115224720

---
+++ 02-code/02-unique-ptr.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+
+class Test
+{
+public:
+    Test(string s)  {
+        str = s;
+        cout << "Test create\n";
+    }
+    ~Test()
+    {
+        cout << "Test delete:" << str << endl;
+    }
+    string& getStr() {
+        return str;
+    }
+    void setStr(string s)  {
+        str = s;
+    }
+    void print()  {
+        cout << str << endl;
+    }
+private:
+    string str;
+};
+
+int main()
+{
+    auto_ptr<Test> ptest(new Test("123"));
+    ptest->setStr("hello ");
+    ptest->print();
+    ptest.get()->print();
+    ptest->getStr() += "world !";
+    (*ptest).print();
+    // 重新绑定指向的对象，而原来的对象则会被释放
+    ptest.reset(new Test("123"));  // 此时会释放ptest之前指向的hello world
+    ptest->print();
+    return 0;
+}
---
+++ 02-code/Cpp14-11-Demo/06-move.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+//void process_value(int& i)
+//{
+//	std::cout << "LValue processed: " << i << std::endl;
+//}
+//
+//void process_value(int&& i)
+//{
+//	std::cout << "RValue processed: " << i << std::endl;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	process_value(a);
+//	process_value(1);
+//}
+
+void process_value(int& i)
+{
+	std::cout << "LValue processed: " << i << std::endl;
+}
+
+void process_value(int&& i)
+{
+	std::cout << "RValue processed: " << std::endl;
+}
+
+int main()
+{
+	int a = 0;
+	process_value(a);
+	int&& x = 3;
+	process_value(x);
+}
+
+
